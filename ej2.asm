219b05cc4d607a670c2efa30012da92bdb469fbb
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-05-19 20:48:33 -0300

    ej2.asm

--- ej2.asm
+++ ej2.asm
@@ -20,19 +20,19 @@
 .text
 .global main
 	main:
-		ldr r0, =A 	    //cargo en r0 la direccion de var1
-		ldr r1, [r0] 	//cargo en r1  el valor de la direccion de memoria de r0
+		ldr r0, =A 	     //cargo en r0 la direccion de A
+		ldr r1, [r0] 	 //cargo en r1  el valor de la direccion de memoria de r0
 		
-		ldr r0, =B
-		ldr r2, [r0]
+		ldr r0, =B       //cargo en r0 la direccion de B
+		ldr r2, [r0]     //cargo en r2  el valor de la direccion de memoria de r0
 
-		ldr r0, =C
-		ldr r3, [r0]
+		ldr r0, =C       //cargo en r0 la direccion de C
+		ldr r3, [r0]     //cargo en r3  el valor de la direccion de memoria de r0
 
         cmp r1, r2       //compara r1 con r2
+        beq end          // var1 == var2 hacer el salto a #equal
 		bgt adition      // var1 > var2 hacer el salto a #adition
-		bal subtraction  // var1 < var2 hacer el salto a #adition
-        end              // var1 == var2 hacer el salto a #equal
+		bal subtraction  // var1 < var2 hacer el salto a #subtraction
 
         adition:
 			add r3, r1, r2 //result = var1 + var2
