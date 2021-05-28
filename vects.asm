ba80451dbc2b978115bb46a4f8c994097cdb712c
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-05-28 19:56:24 -0300

    add vects

--- vects.asm
+++ vects.asm
@@ -5,14 +5,14 @@
 .text
 .global main
 	main:
-        ldr r0, =values    @cargo en r0 la direccion de values
-		ldr r1, [r0] 	   @cargo en r1  el valor de la direccion de memoria de r0
+        ldr r0, =values
+		ldr r1, [r0] 
 
-        ldr r0, =total 	   @cargo en r0 la direccion de total
-		ldr r2, [r0] 	   @cargo en r1  el valor de la direccion de memoria de r0
+        ldr r0, =total 	   
+		ldr r2, [r0] 	   
 
-        ldr r0, =iterator  @cargo en r0 la direccion de total
-		ldr r3, [r0] 	   @cargo en r1  el valor de la direccion de memoria de r0
+        ldr r0, =iterator  
+		ldr r3, [r0] 	   
         
         bal loop
 
@@ -28,11 +28,11 @@
             bal end
         
 		end:
-            mov r7, #4         @saldia por pantalla
-            mov r0, #1         @salida cadena
-            mov r2, #35        @tamaño de la cadena
-            ldr r1, =total
-            swi 0              @ swi, software interrupt
+            mov r7, #4         
+            mov r0, #1         
+            mov r2, #35        
+            ldr r1, =r2
+            swi 0           
 
 			mov r7, #1
 			swi 0
