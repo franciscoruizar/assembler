3274676bda638f9847078ca257ffbc4eaf70c7bc
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-05-30 19:51:37 -0300

    fix error

--- caesar-cipher/ascii.asm
+++ caesar-cipher/ascii.asm
@@ -1,26 +1,6 @@
-/*
-sumar_ascii_minus(character, clave):
-    aux = character + clave
-    if(aux > 122)
-        return aux - 26
-    else
-        return aux
-
-sumar_ascii_mayus(character, clave):
-    aux = character + clave
-
-    if(aux > 90)
-        return aux - 26
-    else
-        return aux
-
-
-
- */
-
 .data
-    character .ascii "a"
-    clave .ascii "2"
+    character: .ascii "a"
+    clave:     .ascii "2"
 .text
 .global main
 
