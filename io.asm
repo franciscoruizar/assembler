df7d4d9e9c832b0d395245532034939abf660caa
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-05-30 16:04:50 -0300

    add caesar-cipher

--- io.asm
+++ io.asm
@@ -2,30 +2,56 @@
     mensaje: .ascii "Ingresar texto de 4 caracteres!: \n"
     cadena:  .ascii " "
 .text
-.global main
 
-main:
 
-    // Salida por pantalla
-    mov r7, #4         @saldia por pantalla
-    mov r0, #1         @salida cadena
-    mov r2, #35        @tamaño de la cadena
-    ldr r1, =mensaje
-    swi 0              @ swi, software interrupt
-
-    // Leer por teclado
-    mov r7, #3      @lectura por teclado
-    mov r0, #0      @ingreso de cadena
-    mov r2, #4      @leer cant caracteres
-    ldr r1, =cadena @donde se guarda lo ingresado
-    swi 0           @ swi, software interrupt
-
-    // Salida por pantalla
-    mov r7, #4         @saldia por pantalla
-    mov r0, #1         @salida cadena
-    mov r2, #35        @tamaño de la cadena
-    ldr r1, =cadena
-    swi 0              @ swi, software interrupt
     
-    mov r7, #1
-    swi 0
+    /*
+    Imprime por consola el valor de r0  
+
+    @ param r0
+    @ return: void
+    */
+    print:
+        .fnstart
+            mov r7, #4         @salida por pantalla
+            mov r0, #1         @salida cadena
+            mov r2, #35        @tamaño de la cadena
+            ldr r1, =mensaje
+            swi 0              @ swi, software interrupt
+            bx lr
+        .fnend
+
+    /*
+    Lee por consola input ingresado por el usuario y retorna el valor en r0
+
+    @ return: r0
+    */
+    input:
+        mov r7, #3      @lectura por teclado
+        mov r0, #0      @ingreso de cadena
+        mov r2, #4      @leer cant caracteres
+        ldr r0, =cadena @donde se guarda lo ingresado
+        swi 0           @ swi, software interrupt
+        bx lr
+
+.global main
+    main:
+
+        // Salida por pantalla
+
+
+        // Leer por teclado
+        
+
+        // Salida por pantalla
+        mov r7, #4         @saldia por pantalla
+        mov r0, #1         @salida cadena
+        mov r2, #35        @tamaño de la cadena
+        ldr r1, =cadena
+        swi 0              @ swi, software interrupt
+        
+        bal end
+        
+    end:
+        mov r7, #1
+        swi 0
