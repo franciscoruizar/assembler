8bc0f075f228a961929dd78d8c973cc101451a70
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-05-30 16:15:27 -0300

    add ToDo

--- caesar-cipher/main.asm
+++ caesar-cipher/main.asm
@@ -3,6 +3,31 @@
         - Parametros para subrutinas: r0 a r3
         - Retornos para subrutinas: r0 a r1
         - Cada subrutina debe tener docs de parametros, retornos y funcion
+
+    ToDo:
+        Backlog:
+            - crear subrutina extraer_mensaje
+            - crear subrutina extraer_clave -> tambien extrae la palabra clave
+            - crear subrutina extraer_opcion
+            - crear subrutina codificar
+            - crear subrutina decodificar
+            - crear subrutina decodificar_con_palabra_clave
+            - crear subrutina length
+            - crear subrutina convertir_ascii_entero
+            - crear subrutina convertir_entero_ascii
+            - crear subrutina es_minuscula
+            - crear subrutina es_mayuscula
+            - crear subrutina es_mayuscula
+            - crear subrutina sumar_ascii_minus
+            - crear subrutina sumar_ascii_mayus
+            - crear logica de opciones
+            - unir subrutinas
+            
+        Doing:
+            - crear subrutina print
+            - crear subrutina input
+        Do:
+            -
  */
 
 
@@ -33,12 +58,14 @@
     @ return: r0
     */
     input:
-        mov r7, #3      @lectura por teclado
-        mov r0, #0      @ingreso de cadena
-        mov r2, #4      @leer cant caracteres
-        ldr r0, =cadena @donde se guarda lo ingresado
-        swi 0           @ swi, software interrupt
-        bx lr
+        .fnstart
+            mov r7, #3      @lectura por teclado
+            mov r0, #0      @ingreso de cadena
+            mov r2, #4      @leer cant caracteres
+            ldr r0, =cadena @donde se guarda lo ingresado
+            swi 0           @ swi, software interrupt
+            bx lr
+        .fnend
 
 
 .global main
