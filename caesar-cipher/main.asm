45b7220b63befb5be09e070250f110d1834de4c7
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-05-30 18:31:19 -0300

    fix error

--- caesar-cipher/main.asm
+++ caesar-cipher/main.asm
@@ -66,7 +66,7 @@
             mov r7, #3           @lectura por teclado
             mov r0, #0           @ingreso de cadena
             mov r2, #200         @leer cant caracteres
-            ldr r0, =cadena      @donde se guarda lo ingresado
+            ldr r1, =cadena      @donde se guarda lo ingresado
             swi 0                @swi, software interrupt
             bx lr
         .fnend
