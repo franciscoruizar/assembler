b09b1f14439e4bb43f9d88f8341132f8ed93c72a
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-06-05 11:39:02 -0300

    add decipher into main.s

--- caesar-cipher/main.s
+++ caesar-cipher/main.s
@@ -368,21 +368,137 @@
         .fnend
 
 
+/*
+        @param r0: caracter encriptado
+        @param r1: clave
+        @return r0: caracter 
+    */
+    desencriptar_caracter:
+        .fnstart
+            push {lr}
+            push {r1}
+            push {r2}
+
+            cmp r0, #0x20
+            moveq r2, r0
+            beq return_caracter_desencriptado
+
+            sub r2, r0, r1                       @desencripto el caracter en base a la clave (caracter encriptado + clave) en r0
+
+            cmp r0, #0x61
+            bge verificar_caracter_encriptado_si_esta_dentro_del_rango_minusculas
+            b verificar_caracter_encriptado_si_esta_dentro_del_rango_mayusculas
+
+            verificar_caracter_encriptado_si_esta_dentro_del_rango_minusculas:
+                cmp r0, #0x7A
+                ble verificar_caracter_desencriptado_overflow_minuscula
+                b return_caracter_desencriptado
+
+            verificar_caracter_encriptado_si_esta_dentro_del_rango_mayusculas:
+                cmp r0, #0x41
+                blt return_caracter_desencriptado
+                cmpge r0, #0x5A
+                ble verificar_caracter_desencriptado_overflow_mayuscula
+                b return_caracter_desencriptado
+
+            verificar_caracter_desencriptado_overflow_minuscula:
+                cmp r2, #0x61
+                blt convertir_caracter_desencriptado_overflow
+                b return_caracter_desencriptado
+
+            verificar_caracter_desencriptado_overflow_mayuscula:
+                cmp r2, #0x41
+                blt convertir_caracter_desencriptado_overflow
+                b return_caracter_desencriptado
+
+            convertir_caracter_desencriptado_overflow:
+                add r2, r2, #26                                    @r2 = r2 + 26
+                b return_caracter_desencriptado                      
+
+            return_caracter_desencriptado:
+                mov r0, r2
+
+                pop {r2}
+                pop {r1}
+                pop {lr}
+                bx lr  
+
+        .fnend
+
+    /*
+        @param r0: direccion de memoria del mensaje encriptado
+        @param r1: clave
+
+        @return cadena desencriptada
+    
+    */
+    desencriptar:
+        .fnstart
+            push {lr}
+            push {r2}
+            push {r3}
+
+            mov r2, r0                         @auxilio la direccion de memoria en r2 
+            desencriptar_loop:
+                ldrb r3, [r2]                  @obtengo el mas signficativo y lo guardo en r3
+                
+                cmp r3, #00                     
+                beq end_desencriptar_loop         @Si r3 == 0, termina la funcion
+                
+                push {r2}
+
+                mov r0, r3                     @Guardo en r0 el caracter 
+                bl desencriptar_caracter           
+
+                pop {r2}
+                strb r0, [r2], #1              @cambio en la posicion del bit mas significativo el caracter encriptado al comun
+
+                b desencriptar_loop
+
+            end_desencriptar_loop:
+                mov r0, r2
+
+                pop {r3}
+                pop {r2}
+                pop {lr}
+                bx lr 
+        .fnend
+
+
 
 .global main
     main:
+        /*                Entrada de datos                     */
         @Entrada de datos
         bl input_usuario
 
         @Proceso y parseo de datos ingresados
         bl parsear_input_usuario
 
-        @Encriptacion
+        /*                Encriptacion                     */
         ldr r0, =mensaje
         ldr r1, =clave_int
         ldr r1, [r1]
 
         bl encriptar
+
+        @Print de la encriptacion
+        mov r2, #10
+        ldr r3, =mensaje
+        bl print
+
+        /*                Desencriptacion                     */
+
+        ldr r0, =mensaje
+        ldr r1, =clave_int
+        ldr r1, [r1]
+
+        bl desencriptar
+
+        @Print de la Desencriptacion
+        mov r2, #10
+        ldr r3, =mensaje
+        bl print
 
         bal end
     end:
